80febd1af01ff0a33f4c51e74c9720438271b930
TomaEduard <eduard.daniel.toma@gmail.com>
2019-09-03 18:09:07 +0300

    added Sprin email dependency, settings and send email to save location

--- src/main/java/com/example/location/contollers/LocationController.java
+++ src/main/java/com/example/location/contollers/LocationController.java
@@ -1,7 +1,6 @@
 package com.example.location.contollers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.location.entities.Location;
-import com.example.location.repos.LocationRepository;
 import com.example.location.service.LocationService;
+import com.example.location.util.EmailUtil;
 
 @Controller
 public class LocationController {
@@ -20,66 +19,73 @@ public class LocationController {
 	@Autowired
 	private LocationService locationService;
 
+	@Autowired
+	EmailUtil emailUtil;
+
 	@RequestMapping("/showCreate")
 	public String showCreate() {
-		
+
 		return "createLocation";
 	}
-	
+
 	@RequestMapping("/saveLoc")
 	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
-		
+
 		Location saveLocation = locationService.saveLocation(location);
 		String msg = "Location saved with id: " + saveLocation.getId();
 		modelMap.addAttribute("msg", msg);
+
+		emailUtil.sendEmail("tester.application.email@gmail.com",
+							"Location Saved",
+							"Location Saved Succesfully and avout to return a response.");
 		
 		return "createLocation";
 	}
-	
+
 	@RequestMapping("/displayLocations")
 	public String displayLocations(ModelMap modelMap) {
-		
+
 		List<Location> locations = locationService.getAllLocations();
 		modelMap.addAttribute("locations", locations);
-		
+
 		return "displayLocations";
 	}
-	
+
 	@RequestMapping("/deleteLocation")
-	public String deleteLocagion(@RequestParam ("id") int id,ModelMap modelMap) {
-		
+	public String deleteLocagion(@RequestParam("id") int id, ModelMap modelMap) {
+
 		// reduct db call, to take the object, we create an object here with the id
 		// another easy way is to change delete method to have int param
 //		Location location = locationService.getLocation(id);
 		Location location = new Location();
 		location.setId(id);
-		
+
 		locationService.deleteLocation(location);
-		
+
 		List<Location> locations = locationService.getAllLocations();
 		modelMap.addAttribute("locations", locations);
-		
+
 		return "displayLocations";
 	}
-	
+
 	@RequestMapping("/showUpdate")
-	public String showUpdatePage(@RequestParam ("id") int id, ModelMap modelMap) {
-		
+	public String showUpdatePage(@RequestParam("id") int id, ModelMap modelMap) {
+
 //		Optional<Location> location = locationService.findById(id);
 		Location location = locationService.getLocationById(id);
 		modelMap.addAttribute("location", location);
-		
+
 		return "updateLocation";
 	}
-	
+
 	@RequestMapping("/updateLoc")
 	public String updateLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
-		
+
 		locationService.updateLocation(location);
-		
+
 		List<Location> allLocations = locationService.getAllLocations();
 		modelMap.addAttribute("locations", allLocations);
-		
+
 		return "displayLocations";
 	}
 }
---
+++ src/main/java/com/example/location/util/EmailUtilImpl.java
@@ -0,0 +1,36 @@
+package com.example.location.util;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtilImpl implements EmailUtil {
+
+	@Autowired
+	JavaMailSender javaMailSender;
+	
+	@Override
+	public void sendEmail(String toAddress, String subject, String body) {
+		 
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		try {
+			helper.setTo(toAddress);
+			helper.setSubject(subject);
+			helper.setText(body);
+		} catch (MessagingException e) {
+			
+			e.printStackTrace();
+		}
+		
+		javaMailSender.send(message);
+	}
+
+
+}
